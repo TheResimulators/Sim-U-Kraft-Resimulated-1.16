2c8331a353236fb333d97f110ba7b947e4c410ce
Aiden <aidenvanzuilen@gmail.com>
2020-01-11 12:25:08 +1300

    Initial building System
    The base interface and start of the builders

---
+++ src/main/java/com/Resimulators/simukraft/common/jobs/core/EnumJobState.java
@@ -0,0 +1,13 @@
+package com.Resimulators.simukraft.common.jobs.core;
+
+public enum EnumJobState {
+        LOOKING_FOR_BLOCK,
+        LOOKING_FOR_RESOURCES,
+        WORKING,
+        RETURNING,
+        NOT_WORKING,
+        GOTO_WORK,
+
+
+
+    }
