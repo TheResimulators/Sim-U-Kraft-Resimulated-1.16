8804e535a6f485240555b4535810ec06c4949c88
Aiden <Aidenvanzuilen@gmail.com>
2020-01-11 22:51:14 +1300

    added start of sim functionality

--- src/main/java/com/Resimulators/simukraft/common/entity/EntitySim.java
+++ src/main/java/com/Resimulators/simukraft/common/entity/EntitySim.java
@@ -2,24 +2,30 @@ package com.Resimulators.simukraft.common.entity;
 
 import com.Resimulators.simukraft.Reference;
 import com.Resimulators.simukraft.init.ModEntities;
+import com.sun.istack.internal.NotNull;
 import net.minecraft.entity.*;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.ICapabilityProvider;
-import net.minecraftforge.common.util.LazyOptional;
 
-import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
 
 public class EntitySim extends AgeableEntity implements INPC {
-    ResourceLocation skin_texture;
+    public static final DataParameter<Float> HUNGER = EntityDataManager.createKey(EntitySim.class, DataSerializers.FLOAT);
+    public static final DataParameter<Integer> PROFESSION = EntityDataManager.createKey(EntitySim.class, DataSerializers.VARINT);
+    public static final DataParameter<Boolean> FEMALE = EntityDataManager.createKey(EntitySim.class, DataSerializers.BOOLEAN);
+    public static final DataParameter<Integer> VARIATION = EntityDataManager.createKey(EntitySim.class, DataSerializers.VARINT);
+
+
+
+    private ResourceLocation skin_texture;
     private static final int SIM_TEXTURE_COUNT = 6;
     public EntitySim(EntityType<? extends AgeableEntity> type, World worldIn) {
         super(ModEntities.ENTITY_SIM, worldIn);
@@ -31,30 +37,33 @@ public class EntitySim extends AgeableEntity implements INPC {
         return this.skin_texture;
     }
 
+    @Override
+    protected void registerData(){
+        this.dataManager.register(HUNGER,20.0f);
+        this.dataManager.register(PROFESSION,0);
+        this.dataManager.register(FEMALE,false);
+        this.dataManager.register(VARIATION,0);
+
+    }
+
 
     @Override
     protected void registerGoals(){
         this.goalSelector.addGoal(0,new SwimGoal(this));
-        this.goalSelector.addGoal(1,new RandomWalkingGoal(this,0.7d));
+        this.goalSelector.addGoal(1,new WaterAvoidingRandomWalkingGoal(this,0.7d));
         this.goalSelector.addGoal(2, new LookAtGoal(this, PlayerEntity.class,0.5f));
         this.goalSelector.addGoal(3,new LookRandomlyGoal(this));
-<<<<<<< HEAD
-=======
+        //4 and 5 reserved for job ai's
+        this.goalSelector.addGoal(4,new OpenDoorGoal(this,true));
 
-
->>>>>>> master
     }
-
-
-
-    @Nullable
+    @ParametersAreNonnullByDefault
     @Override
     public AgeableEntity createChild(AgeableEntity ageable) {
         EntitySim entitySim = new EntitySim(ModEntities.ENTITY_SIM, world);
         entitySim.onInitialSpawn(this.world, this.world.getDifficultyForLocation(new BlockPos(entitySim)), SpawnReason.BREEDING, new AgeableData(), null);
         return entitySim;
     }
-
 
     @Override
     public void deserializeNBT(CompoundNBT nbt) {
@@ -63,6 +72,74 @@ public class EntitySim extends AgeableEntity implements INPC {
 
     @Override
     public CompoundNBT serializeNBT() {
-        return null;
+        CompoundNBT nbt = new CompoundNBT();
+
+
+
+
+
+
+        return nbt;
+    }
+
+    @Override
+    public void notifyDataManagerChange(DataParameter<?> key) {
+        super.notifyDataManagerChange(key);
+    }
+
+
+
+
+    public float getHunger(){
+        return this.dataManager.get(HUNGER);
+    }
+
+    public void setHunger(){
+        this.dataManager.set(HUNGER,20.0f);
+    }
+
+    public int getVariation(){
+        return Math.max(this.dataManager.get(VARIATION),0);
+    }
+    public void setVariation(int variationid){
+       this.dataManager.set(VARIATION,variationid);
+    }
+
+    public void setProfession(int professionid){
+        this.dataManager.set(PROFESSION,0);
+    }
+
+    public int getProfession(){
+        return Math.max(this.dataManager.get(PROFESSION),0);
+    }
+
+    public boolean getFemale (){
+        return this.dataManager.get(FEMALE);
+    }
+    public void setFemale(boolean female){
+        this.dataManager.set(FEMALE,female);
+    }
+
+    public String getLabeledProfession() {
+        switch (getProfession()) {
+            default:
+                return "Nitwit";
+            case 0:
+                return "NitWit";
+            case 1:
+                return "Builder";
+            case 2:
+                return "Farmer";
+            case 3:
+                return "Fisher";
+            case 4:
+                return "Butcher";
+            case 5:
+                return "Cattle Farmer";
+            case 6:
+                return "Sheep Farmer";
+            case 7:
+                return "Miner";
+        }
     }
 }
--- src/main/java/com/Resimulators/simukraft/common/jobs/JobBuilder.java
+++ src/main/java/com/Resimulators/simukraft/common/jobs/JobBuilder.java
@@ -1,9 +1,18 @@
 package com.Resimulators.simukraft.common.jobs;
 
+import com.Resimulators.simukraft.common.entity.EntitySim;
 import com.Resimulators.simukraft.common.jobs.core.EnumJobState;
 import com.Resimulators.simukraft.common.jobs.core.IJob;
+import net.minecraft.entity.ai.goal.Goal;
+import net.minecraft.entity.ai.goal.MoveTowardsTargetGoal;
 
 public class JobBuilder implements IJob {
+    private EntitySim sim;
+    private Goal goal1;
+    JobBuilder(EntitySim sim){
+        this.sim = sim;
+
+    }
     @Override
     public EnumJobState state() {
         return EnumJobState.NOT_WORKING;
@@ -31,11 +40,11 @@ public class JobBuilder implements IJob {
 
     @Override
     public void addJobAi() {
-
+        sim.goalSelector.addGoal(4,goal1);
     }
 
     @Override
     public void removeJobAi() {
-
+        sim.goalSelector.removeGoal(goal1);
     }
 }
