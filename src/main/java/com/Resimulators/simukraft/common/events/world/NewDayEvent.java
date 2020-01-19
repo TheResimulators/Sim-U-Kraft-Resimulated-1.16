6e6671e2e30849798b2345a914691c1e7d41d6f9
Aiden <aidenvanzuilen@gmail.com>
2020-01-20 09:38:19 +1300

    New Day Event

--- src/main/java/com/Resimulators/simukraft/common/events/world/NewDayEvent.java
+++ src/main/java/com/Resimulators/simukraft/common/events/world/NewDayEvent.java
@@ -6,21 +6,25 @@ import net.minecraftforge.common.util.INBTSerializable;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-public class NewDayEvent implements INBTSerializable<CompoundNBT>{
+import java.time.DayOfWeek;
+
+public class NewDayEvent implements INBTSerializable<CompoundNBT> {
+    private static DayOfWeek dayOfWeek;
     private double day = 0;
     private double previousDay = 0;
 
     @SubscribeEvent
-    public void OnNewDayEvent(TickEvent.WorldTickEvent event){
-        if (event.phase == TickEvent.Phase.END){
-           double time =  event.world.getDayTime();
-           day = Math.floor(time /24000);
+    public void OnNewDayEvent(TickEvent.WorldTickEvent event) {
+        if (event.phase == TickEvent.Phase.END) {
+            double time = event.world.getDayTime();
+            day = Math.floor(time / 24000);
+            dayOfWeek = DayOfWeek.of((int)(1+(day % 7)));
 
-           if (day != previousDay){
-               payRent();
-               previousDay = day;
-               
-           }
+            if (day != previousDay) {
+                payRent();
+                previousDay = day;
+
+            }
         }
 
 
@@ -29,21 +33,25 @@ public class NewDayEvent implements INBTSerializable<CompoundNBT>{
     @Override
     public CompoundNBT serializeNBT() {
         CompoundNBT nbt = new CompoundNBT();
-        nbt.putDouble("day",day);
-        nbt.putDouble("previousDay",previousDay);
+        nbt.putDouble("day", day);
+        nbt.putDouble("previousDay", previousDay);
 
         return nbt;
     }
 
-        @Override
-        public void deserializeNBT(CompoundNBT nbt) {
-            day = nbt.getDouble("day");
-            previousDay = nbt.getDouble("previousDay");
-        }
+    @Override
+    public void deserializeNBT(CompoundNBT nbt) {
+        day = nbt.getDouble("day");
+        previousDay = nbt.getDouble("previousDay");
+    }
 
 
-    public void payRent(){
+    public void payRent() {
 
 
+    }
+
+    public static DayOfWeek getDay() {
+        return dayOfWeek;
     }
 }
