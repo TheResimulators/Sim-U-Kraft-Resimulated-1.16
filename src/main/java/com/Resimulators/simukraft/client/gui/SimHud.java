160790d773d007df597e45cf66635fa91e79dbc7
Aiden <Aidenvanzuilen@gmail.com>
2020-01-22 12:55:45 +1300

    Hiring,Spawn Sync to client

--- src/main/java/com/Resimulators/simukraft/client/gui/SimHud.java
+++ src/main/java/com/Resimulators/simukraft/client/gui/SimHud.java
@@ -2,6 +2,7 @@ package com.Resimulators.simukraft.client.gui;
 
 import com.Resimulators.simukraft.common.capabilities.PlayerCapability;
 import com.Resimulators.simukraft.common.events.world.NewDayEvent;
+import com.Resimulators.simukraft.common.world.SavedWorldData;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.AbstractGui;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
@@ -17,12 +18,9 @@ public class SimHud extends AbstractGui {
     @SubscribeEvent
     public void render(RenderGameOverlayEvent.Text event){
         if (Minecraft.getInstance().currentScreen == null) {
-            LazyOptional<PlayerCapability> cap = Minecraft.getInstance().player.getCapability(PlayerCapability.Provider.TEST);
-            cap.ifPresent(playerCapability -> {
-
-                num = playerCapability.getFaction().getAmountOfSims();
-                credits = playerCapability.getFaction().getCredits();
-            });
+           SavedWorldData data = SavedWorldData.get(Minecraft.getInstance().world);
+            num = data.getFactionWithPlayer(Minecraft.getInstance().player.getUniqueID()).getAmountOfSims();
+            credits = data.getFactionWithPlayer(Minecraft.getInstance().player.getUniqueID()).getCredits();
             String day = NewDayEvent.getDay().getDisplayName(TextStyle.FULL, Locale.ENGLISH);
             Minecraft.getInstance().fontRenderer.drawString("Sims: " + num, 10, 10, 16777215);
             Minecraft.getInstance().fontRenderer.drawString("Credits: " + String.format("%.2f", credits), 10, 30, 16777215);
