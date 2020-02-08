b517618d9fb7a3758117d74d0a33122792c0821a
aidie8 <aidenvanzuilen@gmail.com>
2020-02-08 21:47:34 +1300

    Loot Tables, Marker model (#50)
    * Marker Block Added
    
    * Loot Table Generation
    
    * Loot Tables for all blocks
    
    * Fixed Tile Entity Error when destroyed
    
    * Fix Errors

--- src/main/java/com/Resimulators/simukraft/client/gui/BuilderGui.java
+++ src/main/java/com/Resimulators/simukraft/client/gui/BuilderGui.java
@@ -4,6 +4,7 @@ import com.Resimulators.simukraft.Reference;
 import com.Resimulators.simukraft.common.world.Structure;
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.widget.button.Button;
@@ -13,6 +14,7 @@ import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.text.ITextComponent;
 
 import javax.annotation.Nullable;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class BuilderGui extends BaseJobGui {
@@ -20,6 +22,7 @@ public class BuilderGui extends BaseJobGui {
     private Button CustomBack;
     private boolean loaded = false;
     private ArrayList<Structure> structures;
+
     public BuilderGui(ITextComponent component, ArrayList<Integer> ids, BlockPos pos, @Nullable int id) {
         super(component, ids, pos, id);
         this.job = "builder";
@@ -29,45 +32,53 @@ public class BuilderGui extends BaseJobGui {
     @Override
     public void init(Minecraft minecraft, int width, int height) {
         super.init(minecraft, width, height);
-        addButton(Build= new LargeButton(width/2-55,height-55,110,42,"Build",(Build->{
-            super.hideAll();
-            CustomBack.visible = true;
-            state = State.SELECTBULDING;
-        })));
-        //Build.active=false;
-        addButton(CustomBack = new Button(width-120,height-30,110,20,"Back",(Back->{
-            super.Back.onPress();
-            if (state == State.SELECTBULDING){
-                state = State.MAIN;
-                showMainMenu();
-
-            }
-            if (state == State.BUILDINGINFO){
+        if (loaded) {
+            addButton(Build = new LargeButton(width / 2 - 55, height - 55, 110, 42, "Build", (Build -> {
+                super.hideAll();
+                CustomBack.visible = true;
                 state = State.SELECTBULDING;
+            })));
+            //Build.active=false;
+            addButton(CustomBack = new Button(width - 120, height - 30, 110, 20, "Back", (Back -> {
+                super.Back.onPress();
+                if (state == State.SELECTBULDING) {
+                    state = State.MAIN;
+                    showMainMenu();
+
+                }
+                if (state == State.BUILDINGINFO) {
+                    state = State.SELECTBULDING;
+                }
+
             }
 
+
+            )));
         }
-
-
-
-
-        )));
     }
 
-
-    public void setStructures(ArrayList<Structure> structures){
+    public void loadBuildings(ArrayList<Structure> structures) {
+        this.loaded = true;
         this.structures = structures;
-        
+        init();
+    }
+
+    public void setStructures(ArrayList<Structure> structures) {
+        this.structures = structures;
+
     }
 
     @Override
     public void render(int p_render_1_, int p_render_2_, float p_render_3_) {
+        if (loaded) super.render(p_render_1_, p_render_2_, p_render_3_);
+        else {
+            font.drawString("Loading", (float) width / 2 - font.getStringWidth("Loading"), (float) height / 2, Color.WHITE.getRGB());
+        }
 
-        super.render(p_render_1_,p_render_2_,p_render_3_);
     }
 
     @Override
-    public void showMainMenu(){
+    public void showMainMenu() {
         super.showMainMenu();
         Build.visible = true;
     }
@@ -79,17 +90,17 @@ public class BuilderGui extends BaseJobGui {
 
     }
 
-    private class LargeButton extends Button{
-        final ResourceLocation LARGE_BUTTON = new ResourceLocation(Reference.MODID,"textures/gui/large_button.png");
+    private class LargeButton extends Button {
+        final ResourceLocation LARGE_BUTTON = new ResourceLocation(Reference.MODID, "textures/gui/large_button.png");
+
         public LargeButton(int widthIn, int heightIn, int width, int height, String text, IPressable onPress) {
             super(widthIn, heightIn, width, height, text, onPress);
 
         }
 
 
-
         @Override
-        public void renderButton(int p_renderButton_1_, int p_renderButton_2_, float p_renderButton_3_){
+        public void renderButton(int p_renderButton_1_, int p_renderButton_2_, float p_renderButton_3_) {
             Minecraft minecraft = Minecraft.getInstance();
             FontRenderer fontrenderer = minecraft.fontRenderer;
             minecraft.getTextureManager().bindTexture(LARGE_BUTTON);
@@ -98,8 +109,8 @@ public class BuilderGui extends BaseJobGui {
             RenderSystem.enableBlend();
             RenderSystem.defaultBlendFunc();
             RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
-            this.blit(this.x, this.y, 0,  1+i *42, this.width / 2, this.height);
-            this.blit(this.x + this.width / 2, this.y, 200 - this.width / 2,  1+i * 42, this.width / 2, this.height);
+            this.blit(this.x, this.y, 0, 1 + i * 42, this.width / 2, this.height);
+            this.blit(this.x + this.width / 2, this.y, 200 - this.width / 2, 1 + i * 42, this.width / 2, this.height);
             this.renderBg(minecraft, p_renderButton_1_, p_renderButton_2_);
             int j = getFGColor();
             this.drawCenteredString(fontrenderer, this.getMessage(), this.x + this.width / 2, this.y + (this.height - 8) / 2, j | MathHelper.ceil(this.alpha * 255.0F) << 24);
@@ -107,11 +118,8 @@ public class BuilderGui extends BaseJobGui {
         }
 
 
-
-
-
         @Override
-        public int getYImage(boolean hovered){
+        public int getYImage(boolean hovered) {
             int i = 0;
             if (!this.active) {
                 i = 1;
@@ -121,7 +129,7 @@ public class BuilderGui extends BaseJobGui {
 
             return i;
         }
-        }
-
-
     }
+
+
+}
--- src/main/java/com/Resimulators/simukraft/init/ModBlocks.java
+++ src/main/java/com/Resimulators/simukraft/init/ModBlocks.java
@@ -8,6 +8,7 @@ import net.minecraft.block.CommandBlockBlock;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
 import net.minecraft.item.DyeColor;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.registries.ObjectHolder;
 
@@ -19,18 +20,20 @@ import java.util.Map;
 @ObjectHolder(Reference.MODID)
 public final class ModBlocks {
     private static List<Block> REGISTRY = new ArrayList<>();
-
     //Basic Blocks
     public static final Block COMPOSITE_BRICK = register(new BlockBase(Block.Properties.create(Material.ROCK,MaterialColor.STONE),"composite_brick"));
     public static final Block CHEESE_BLOCK = register(new BlockBase(Block.Properties.create(Material.SPONGE,MaterialColor.YELLOW),"cheese_block"));
 
     //Sim-u-kraft Structure Blocks
     public static Map<String, Block> LIGHT_BLOCKS = new HashMap<>();
-    public static final Block CONSTRUCTOR_BOX = register(new BlockConstructor(Block.Properties.create(Material.WOOD, MaterialColor.BROWN),"constructor_box"));
-    public static final Block CONTROL_BOX = register(new BlockControlBox(Block.Properties.create(Material.ROCK,MaterialColor.GRAY),"control_box"));
+    public static final Block CONSTRUCTOR_BOX = register(new BlockConstructor(Block.Properties.create(Material.WOOD, MaterialColor.BROWN).harvestTool(ToolType.AXE),"constructor_box"));
+    public static final Block CONTROL_BOX = register(new BlockControlBox(Block.Properties.create(Material.ROCK,MaterialColor.GRAY).harvestTool(ToolType.PICKAXE),"control_box"));
     public static final Block FARM_BOX = register(new BlockFarmBox(Block.Properties.create(Material.ROCK,MaterialColor.GRAY),"farm_box"));
     public static final Block RAINBOW_LIGHT = register(new BlockLight(Block.Properties.create(Material.WOOL),"rainbow_light"));
     public static final Block MINE_BOX = register(new BlockMineBox(Block.Properties.create(Material.WOOD),"mine_box"));
+
+    //special blocks
+    public static final Block MARKER = register(new BlockMarker(Block.Properties.create(Material.WOOD).harvestTool(ToolType.AXE).harvestLevel(1),"marker"));
 
     public ModBlocks() {
         for (DyeColor color : DyeColor.values()) {
