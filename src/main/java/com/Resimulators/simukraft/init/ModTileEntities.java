bc1e114f31d0cf83a92d9faae220dd05c5d3a239
Benjamin <theslimeslayer04@gmail.com>
2020-02-08 12:47:39 -0500

    Spawning fix (again) and started TileMiner

--- src/main/java/com/Resimulators/simukraft/init/ModTileEntities.java
+++ src/main/java/com/Resimulators/simukraft/init/ModTileEntities.java
@@ -2,6 +2,7 @@ package com.Resimulators.simukraft.init;
 
 import com.Resimulators.simukraft.Reference;
 import com.Resimulators.simukraft.common.tileentity.TileConstructor;
+import com.Resimulators.simukraft.common.tileentity.TileMiner;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
 
@@ -12,6 +13,7 @@ public class ModTileEntities {
     private static List<TileEntityType> REGISTRY = new ArrayList<>();
 
     public static final TileEntityType<TileConstructor> CONSTRUCTOR = register(TileEntityType.Builder.create(TileConstructor::new, ModBlocks.CONSTRUCTOR_BOX), "constructor");
+    public static final TileEntityType<TileMiner> MINER = register(TileEntityType.Builder.create(TileMiner::new, ModBlocks.MINE_BOX), "miner");
 
     private static <T extends TileEntity> TileEntityType<T> register(TileEntityType.Builder<T> builder, String name) {
         TileEntityType<T> type = builder.build(null);
