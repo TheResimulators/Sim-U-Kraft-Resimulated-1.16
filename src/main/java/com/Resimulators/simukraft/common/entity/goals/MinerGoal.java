62763e4c964761fca4fead13a1aacbe177f4b278
Aiden <Aidenvanzuilen@gmail.com>
2020-04-02 22:52:41 +1300

    Fixed formating on MinerGoal

--- src/main/java/com/Resimulators/simukraft/common/entity/goals/MinerGoal.java
+++ src/main/java/com/Resimulators/simukraft/common/entity/goals/MinerGoal.java
@@ -23,7 +23,8 @@ public class MinerGoal extends Goal {
     private Direction dir;
 
     private int progress;
-    public MinerGoal(EntitySim sim){
+
+    public MinerGoal(EntitySim sim) {
         job = sim.getJob();
         this.sim = sim;
     }
@@ -33,19 +34,18 @@ public class MinerGoal extends Goal {
 
         job = sim.getJob();
         if (job == null) return false;
-        if (job.getWorkSpace() == null)return false;
-        if ( ((TileMiner)sim.world.getTileEntity(job.getWorkSpace())).getMarker() == null) return false;
+        if (job.getWorkSpace() == null) return false;
+        if (((TileMiner) sim.world.getTileEntity(job.getWorkSpace())).getMarker() == null) return false;
         if (!((TileMiner) sim.world.getTileEntity(job.getWorkSpace())).CheckValidity()) return false;
-        if (job.getWorkSpace() == null)return false;
+        if (job.getWorkSpace() == null) return false;
         if (sim.world.getTileEntity(job.getWorkSpace()) == null) return false;
-        if (true)return true;
-        if (job.getState() == EnumJobState.GOING_TO_WORK){
-            if (sim.getPosition().withinDistance(new Vec3i(job.getWorkSpace().getX(),job.getWorkSpace().getY(),job.getWorkSpace().getZ()),5)){
+        if (true) return true;
+        if (job.getState() == EnumJobState.GOING_TO_WORK) {
+            if (sim.getPosition().withinDistance(new Vec3i(job.getWorkSpace().getX(), job.getWorkSpace().getY(), job.getWorkSpace().getZ()), 5)) {
                 job.setState(EnumJobState.WORKING);
                 return true;
             }
         }
-
 
 
         return false;
@@ -53,18 +53,18 @@ public class MinerGoal extends Goal {
 
 
     @Override
-    public void startExecuting(){
-        TileMiner miner = ((TileMiner)sim.world.getTileEntity(job.getWorkSpace()));
-        progress = ((JobMiner)sim.getJob()).getProgress();
+    public void startExecuting() {
+        TileMiner miner = ((TileMiner) sim.world.getTileEntity(job.getWorkSpace()));
+        progress = ((JobMiner) sim.getJob()).getProgress();
         markerPos = miner.getMarker();
-        dir =  miner.getDir();
+        dir = miner.getDir();
         offset = BlockPos.ZERO.offset(dir).offset(dir.rotateY());
-        width = miner.getWidth()-1;
-        depth = miner.getDepth()-1;
-        for (int x = 0;x< width;x++){
-            for (int z = 0;z<depth;z++){
-                sim.world.setBlockState(markerPos.add(offset.getX(),offset.getY()+4,offset.getZ()).offset(dir,z).offset(dir.rotateY(),x), Blocks.COBBLESTONE.getDefaultState());
-                BlockPos pos = markerPos.add(offset.getX(),offset.getY()+4,offset.getZ()).offset(dir,z).offset(dir.rotateY(),x);
+        width = miner.getWidth() - 1;
+        depth = miner.getDepth() - 1;
+        for (int x = 0; x < width; x++) {
+            for (int z = 0; z < depth; z++) {
+                sim.world.setBlockState(markerPos.add(offset.getX(), offset.getY() + 4, offset.getZ()).offset(dir, z).offset(dir.rotateY(), x), Blocks.COBBLESTONE.getDefaultState());
+                BlockPos pos = markerPos.add(offset.getX(), offset.getY() + 4, offset.getZ()).offset(dir, z).offset(dir.rotateY(), x);
                 System.out.println(pos);
             }
         }
@@ -78,21 +78,21 @@ public class MinerGoal extends Goal {
     }
 
 
-
     @Override
-    public boolean shouldContinueExecuting(){
-        if (sim.getJob() != null){
-        if (sim.getJob().getState() == EnumJobState.FORCE_STOP){
-            ((JobMiner)job).setProgress(progress);
-            return false;
+    public boolean shouldContinueExecuting() {
+        if (sim.getJob() != null) {
+            if (sim.getJob().getState() == EnumJobState.FORCE_STOP) {
+                ((JobMiner) job).setProgress(progress);
+                return false;
+            }
+            if (tick < sim.getJob().workTime()) {
+                return true;
+            } else {
+                sim.getJob().finishedWorkPeriod();
+                sim.getJob().setState(EnumJobState.NOT_WORKING);
+            }
         }
-        if (tick<sim.getJob().workTime()){
-            return true;
-        }else{
-            sim.getJob().finishedWorkPeriod();
-            sim.getJob().setState(EnumJobState.NOT_WORKING);
-        }}
-        ((JobMiner)job).setProgress(progress);
+        ((JobMiner) job).setProgress(progress);
         return false;
     }
 
