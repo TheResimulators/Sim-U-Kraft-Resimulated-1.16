d236797eb9de59cc95654368cf793a3630e9e513
William <fabbe50@users.noreply.github.com>
2020-07-18 19:41:30 +0200

    Cleanup and Proxies

---
+++ src/main/java/com/resimulators/simukraft/proxy/ClientProxy.java
@@ -0,0 +1,18 @@
+package com.resimulators.simukraft.proxy;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+
+public class ClientProxy implements IProxy {
+    @Override
+    public World getClientWorld() {
+        return Minecraft.getInstance().world;
+    }
+
+    @Override
+    public PlayerEntity getClientPlayer() {
+        return Minecraft.getInstance().player;
+    }
+}
---
+++ src/main/java/com/resimulators/simukraft/proxy/ServerProxy.java
@@ -0,0 +1,16 @@
+package com.resimulators.simukraft.proxy;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+
+public class ServerProxy implements IProxy {
+    @Override
+    public World getClientWorld() {
+        throw new IllegalStateException("Only run this on the client");
+    }
+
+    @Override
+    public PlayerEntity getClientPlayer() {
+        throw new IllegalStateException("Only run this on the client");
+    }
+}
