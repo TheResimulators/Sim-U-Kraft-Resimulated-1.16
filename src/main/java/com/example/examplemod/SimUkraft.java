7d050b751cbd8c632c7fd549ca2e2fe5de477291
Aiden <Aidenvanzuilen@gmail.com>
2021-06-25 20:16:01 +1200

    fixed loading of minecraft in dev enviroment and update gradle files to be the same as develop branch

--- src/main/java/com/example/examplemod/SimUkraft.java
+++ src/main/java/com/example/examplemod/SimUkraft.java
@@ -19,13 +19,13 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("simukraft")
+public class SimUkraft
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public SimUkraft() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
@@ -48,7 +48,6 @@ public class ExampleMod
 
     private void doClientStuff(final FMLClientSetupEvent event) {
         // do something that can only be done on the client
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
     }
 
     private void enqueueIMC(final InterModEnqueueEvent event)
