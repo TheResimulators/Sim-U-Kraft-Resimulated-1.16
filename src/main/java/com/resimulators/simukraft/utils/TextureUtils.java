c3fdb145c66faf0078f3be5bfed7fdac6d5454f4
Aiden <Aidenvanzuilen@gmail.com>
2021-12-17 16:51:19 +1300

    Debugging to texture utils

--- src/main/java/com/resimulators/simukraft/utils/TextureUtils.java
+++ src/main/java/com/resimulators/simukraft/utils/TextureUtils.java
@@ -2,6 +2,8 @@ package com.resimulators.simukraft.utils;
 
 import com.resimulators.simukraft.Reference;
 import com.resimulators.simukraft.SimuKraft;
+import net.minecraft.client.Minecraft;
+import net.minecraft.resources.IResource;
 import net.minecraft.util.ResourceLocation;
 
 import java.io.IOException;
@@ -19,14 +21,18 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class TextureUtils {
-    public static List<ResourceLocation> getAllFilesInFolder(String folderLocation) throws URISyntaxException, IOException {
+    public static List<ResourceLocation> getAllFilesInFolder1(String folderLocation) throws URISyntaxException, IOException {
         FileSystem fileSystem;
         final String DEFAULT_PART_OF_PATH = "/assets/simukraft/";
         String stringUrl = DEFAULT_PART_OF_PATH + folderLocation;
         URL url = SimuKraft.class.getResource(stringUrl);
+        SimuKraft.LOGGER().debug("URL of Resource = " + url);
+        System.out.println("URL of Resource = " + url);
         if (url != null) {
             URI uri = url.toURI();
             Path path;
+            SimuKraft.LOGGER().debug("Scheme of Resource = " + url);
+            System.out.println("Scheme of Resource = " + url);
             if ("file".equals(uri.getScheme())) {
                 path = Paths.get(SimuKraft.class.getResource(stringUrl).toURI());
 
@@ -38,4 +44,5 @@ public class TextureUtils {
         }
         return null;
     }
+
 }
